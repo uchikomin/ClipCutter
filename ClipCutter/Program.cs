36df201269ae6868bd3388f50ef19a8fef657114
nakaochi.kohei <nakaochi.kohei@FCCLS0076552>
2025-06-27 12:18:23 +0900

    多重起動を抑止するよう修正

--- ClipCutter/Program.cs
+++ ClipCutter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,20 +11,37 @@ namespace ClipCutter
     internal static class Program
     {
         /// <summary>
+        /// Mutex 名
+        /// </summary>
+        private static readonly string AppMutexName = "4c439d3c-9b29-463d-8f55-5dc36dbbaa4a";
+
+        /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormMain());
+            // 新規作成フラグ
+            bool createdNew = false;
 
-            // メイン処理を初期化
-            new MainClass();
+            // Mutex 作成
+            using (var mutex = new Mutex(true, AppMutexName, out createdNew))
+            {
+                // すでに作成されている場合
+                if (!createdNew)
+                {
+                    // 
+                    MessageBox.Show("すでに起動しています");
+                    // 終了
+                    return;
+                }
 
-            // アプリケーション実行
-            Application.Run();
+                // メイン処理を初期化
+                new MainClass();
+
+                // アプリケーション実行
+                Application.Run();
+            }
         }
     }
 }
